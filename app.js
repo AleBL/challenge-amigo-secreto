e8ee9abfe797339eb90c04e0707d3b88274c1ae9
Alessandro B <ale_alessandro_ale@hotmail.com>
2025-02-06 21:55:52 -0300

    implementation of the function to add friends

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-let amigos = []
+let friendList = [];
+
+addFriend = () => {
+    const inputElement = document.getElementById('friendInput');
+    const friendName = inputElement.value;
+
+    if (friendName === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+
+    friendList.push(friendName);
+
+    inputElement.value = '';
+    updateFriendList();
+}
+
